312dc632739a363dd1e67b3093a9f70f0bfce268
Marcos Souza <marcos.dev.ti@gmail.com>
2020-02-09 03:56:43 -0500

    api address

--- src/TouchOnline.Api/ClientApp/src/environments/environment.ts
+++ src/TouchOnline.Api/ClientApp/src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'http://localhost:5000/api/lessons',
-  apiAuth: 'http://localhost:5000/api/auth/',
+  apiUrl: 'http://vps281295.vps.ovh.ca/api/lessons',
+  apiAuth: 'http://vps281295.vps.ovh.ca/api/auth/',
   production: false
 };
 
