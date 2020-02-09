614a32a16cc8693ace75478a6b4b6e07de076c58
Marcos Souza <marcos.dev.ti@gmail.com>
2020-02-09 03:45:58 -0500

    cors

--- src/TouchOnline.Api/Startup.cs
+++ src/TouchOnline.Api/Startup.cs
@@ -22,6 +22,7 @@ namespace TouchOnline.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             ToBootstrapper.Register(services, Configuration);
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
@@ -38,6 +39,7 @@ namespace TouchOnline.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
