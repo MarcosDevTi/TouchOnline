fb9e93219e3cd6e0359b46e725e31157211b95a0
Marcos Souza <marcos.dev.ti@gmail.com>
2020-02-09 22:05:57 -0500

    connection strings

--- src/TouchOnline.Data/ToContext.cs
+++ src/TouchOnline.Data/ToContext.cs
@@ -7,7 +7,7 @@ namespace TouchOnline.Data
     {
         public ToContext(DbContextOptions<ToContext> options) : base(options)
         {
-            Database.EnsureCreated();
+            //Database.EnsureCreated();
         }
         public DbSet<Result> Results { get; set; }
         public DbSet<User> Users { get; set; }
--- src/TouchOnline.IoC/ToBootstrapper.cs
+++ src/TouchOnline.IoC/ToBootstrapper.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.IO;
 using TouchOnline.CqrsClient.Contracts;
 using TouchOnline.Data;
 
@@ -10,7 +11,9 @@ namespace TouchOnline.IoC
     {
         public static void Register(IServiceCollection services, IConfiguration config)
         {
-            services.AddDbContext<ToContext>(db => db.UseSqlite(config.GetConnectionString("DefaultConnection")));
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+            var conn = @$"Data Source={path}\Dm.db";
+            services.AddDbContext<ToContext>(db => db.UseSqlite(conn));
             services.AddScoped<IProcessor, Processor>();
             services.AddCqrs();
         }
