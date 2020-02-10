1b572dbf55b0f9059fdb8d613d93be772e765237
Marcos Souza <marcos.dev.ti@gmail.com>
2020-02-09 23:52:59 -0500

    ajustement css

--- src/TouchOnline.Api/ClientApp/src/environments/environment.prod.ts
+++ src/TouchOnline.Api/ClientApp/src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
-  //apiUrl: 'http://vps281295.vps.ovh.ca/api/lessons',
-  //apiAuth: 'http://vps281295.vps.ovh.ca/api/auth/',
-  apiUrl: 'http://localhost:5000/api/lessons',
-  apiAuth: 'http://localhost:5000/api/auth/',
+  apiUrl: 'http://vps281295.vps.ovh.ca/api/lessons',
+  apiAuth: 'http://vps281295.vps.ovh.ca/api/auth/',
   production: true
 };
