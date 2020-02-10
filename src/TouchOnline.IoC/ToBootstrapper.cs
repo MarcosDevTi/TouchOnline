9f50ba3d7aba5ede912b3c0d748f6aa0d276841d
Marcos Souza <marcos.dev.ti@gmail.com>
2020-02-09 19:54:22 -0500

    file bd emplacement

--- src/TouchOnline.IoC/ToBootstrapper.cs
+++ src/TouchOnline.IoC/ToBootstrapper.cs
@@ -2,8 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using TouchOnline.CqrsClient.Contracts;
 using TouchOnline.Data;
 
@@ -14,7 +12,7 @@ namespace TouchOnline.IoC
         public static void Register(IServiceCollection services, IConfiguration config)
         {
             //services.AddDbContext<DmContext>(db => db.UseSqlServer(config.GetConnectionString("DefaultConnection")));
-            services.AddDbContext<ToContext>(db => db.UseSqlite(config.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ToContext>(db => db.UseSqlite(string.Format("Filename={0}/Dm.db", AppDomain.CurrentDomain.BaseDirectory)));
             services.AddScoped<IProcessor, Processor>();
             services.AddCqrs();
         }
