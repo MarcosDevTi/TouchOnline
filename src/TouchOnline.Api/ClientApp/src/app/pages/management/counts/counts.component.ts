e931f9e665967abc4d62c8ce32eab6e656c77181
Marcos Souza <marcos.dev.ti@gmail.com>
2020-03-29 22:58:39 -0400

    auto count

--- src/TouchOnline.Api/ClientApp/src/app/pages/management/counts/counts.component.ts
+++ src/TouchOnline.Api/ClientApp/src/app/pages/management/counts/counts.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ManagementService } from '../shared/management.service';
 import { Counts } from 'src/app/core/navbar/counts';
+
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-counts',
   templateUrl: './counts.component.html',
   styleUrls: ['./counts.component.css']
 })
-export class CountsComponent implements OnInit {
+export class CountsComponent implements OnInit, OnDestroy {
   counts: Counts
+  sub;
   constructor(private managementService: ManagementService) { }
-
   ngOnInit() {
-    this.managementService.GetCounts().subscribe(_ => this.counts = _);
+    this.sub = interval(2000)
+      .subscribe((val) =>
+        this.managementService.GetCounts().subscribe(_ => this.counts = _)
+      );
+
   }
 
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }
