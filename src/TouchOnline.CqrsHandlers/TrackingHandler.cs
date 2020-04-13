f7f1e2df2f0bbcd6bdee96f0a035346b565e9c7a
Marcos Souza <msouza@lgs.com>
2020-04-13 15:52:04 -0400

    .

--- src/TouchOnline.CqrsHandlers/TrackingHandler.cs
+++ src/TouchOnline.CqrsHandlers/TrackingHandler.cs
@@ -42,29 +42,24 @@ namespace TouchOnline.CqrsHandlers
         {
             var result = _context.GetRecordeds
                 .Include(_ => _.User).Include(_ => _.Keyborad)
-                .Where(_ => _.CreateDate > DateTime.Now.Date)
+                .Where(_ => _.CreateDate > query.InitialDate.Date && _.CreateDate < query.InitialDate.Date.AddDays(1))
                 .ToList()
             .GroupBy(_ => _.Ip)
-            .Select(_ =>
+            .Select(_ => new Visitor
             {
-                var first = _.FirstOrDefault();
-                var firstUserNotNull = _.FirstOrDefault(e => e.User != null)?.User;
-                return new Visitor
-                {
-                    Email = firstUserNotNull?.Email,
-                    City = first?.City,
-                    Country = first?.Country,
-                    Region = first?.Region,
-                    KeyboardName = _.FirstOrDefault(_ => _.Keyborad != null)?.Keyborad?.Name,
-                    LanguageBrowser = first?.LanguageBrowser,
-                    LanguageSystem = first?.LanguageSystem,
-                    PagesCount = _.Count(),
-                    ResultCount = _.Count(_ => _.VisitedPages.Contains("result")),
-                    DateCreateUser = firstUserNotNull?.InscriptionDate,
-                    FirstLessonDate = _.Select(_ => _.CreateDate).Min(),
-                    LastLessonDate = _.Select(_ => _.CreateDate).Max(),
-                    CountResultsForUser = firstUserNotNull?.RecordedTrackings?.Count
-                };
+                Email = _.FirstOrDefault(e => e.User != null)?.User.Email,
+                City = _.FirstOrDefault()?.City,
+                Country = _.FirstOrDefault()?.Country,
+                Region = _.FirstOrDefault().Region,
+                KeyboardName = _.FirstOrDefault(_ => _.Keyborad != null)?.Keyborad?.Name,
+                LanguageBrowser = _.FirstOrDefault()?.LanguageBrowser,
+                LanguageSystem = _.FirstOrDefault()?.LanguageSystem,
+                PagesCount = _.Count(),
+                ResultCount = _.Count(_ => _.VisitedPages.Contains("result")),
+                DateCreateUser = _.FirstOrDefault(e => e.User != null)?.User?.InscriptionDate,
+                FirstLessonDate = _.Select(_ => _.CreateDate).Min(),
+                LastLessonDate = _.Select(_ => _.CreateDate).Max(),
+                CountResultsForUser = _.FirstOrDefault(e => e.User != null)?.User.RecordedTrackings.Count
             });
 
             return result;
@@ -73,31 +68,26 @@ namespace TouchOnline.CqrsHandlers
         public IEnumerable<Visitor> Handle(GetTrackingsLastMinute query)
         {
             var result = _context.GetRecordeds
-               .Include(_ => _.User).Include(_ => _.Keyborad)
-               .Where(_ => _.CreateDate > DateTime.Now.AddMinutes(-1))
-               .ToList()
-           .GroupBy(_ => _.Ip)
-           .Select(_ =>
-           {
-               var first = _.FirstOrDefault();
-               var firstUserNotNull = _.FirstOrDefault(e => e.User != null)?.User;
-               return new Visitor
-               {
-                   Email = firstUserNotNull?.Email,
-                   City = first?.City,
-                   Country = first?.Country,
-                   Region = first?.Region,
-                   KeyboardName = _.FirstOrDefault(_ => _.Keyborad != null)?.Keyborad?.Name,
-                   LanguageBrowser = first?.LanguageBrowser,
-                   LanguageSystem = first?.LanguageSystem,
-                   PagesCount = _.Count(),
-                   ResultCount = _.Count(_ => _.VisitedPages.Contains("result")),
-                   DateCreateUser = firstUserNotNull?.InscriptionDate,
-                   FirstLessonDate = _.Select(_ => _.CreateDate).Min(),
-                   LastLessonDate = _.Select(_ => _.CreateDate).Max(),
-                   CountResultsForUser = firstUserNotNull?.RecordedTrackings?.Count
-               };
-           });
+                .Include(_ => _.User).Include(_ => _.Keyborad)
+                .Where(_ => _.CreateDate > DateTime.Now.AddMinutes(-1))
+                .ToList()
+            .GroupBy(_ => _.Ip)
+            .Select(_ => new Visitor
+            {
+                Email = _.FirstOrDefault(e => e.User != null)?.User.Email,
+                City = _.FirstOrDefault()?.City,
+                Country = _.FirstOrDefault()?.Country,
+                Region = _.FirstOrDefault().Region,
+                KeyboardName = _.FirstOrDefault(_ => _.Keyborad != null)?.Keyborad?.Name,
+                LanguageBrowser = _.FirstOrDefault()?.LanguageBrowser,
+                LanguageSystem = _.FirstOrDefault()?.LanguageSystem,
+                PagesCount = _.Count(),
+                ResultCount = _.Count(_ => _.VisitedPages.Contains("result")),
+                DateCreateUser = _.FirstOrDefault(e => e.User != null)?.User?.InscriptionDate,
+                FirstLessonDate = _.Select(_ => _.CreateDate).Min(),
+                LastLessonDate = _.Select(_ => _.CreateDate).Max(),
+                CountResultsForUser = _.FirstOrDefault(e => e.User != null)?.User.RecordedTrackings.Count
+            });
 
             return result;
         }
