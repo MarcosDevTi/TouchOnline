c8ab093ca94543a5c9e139cf0cd7aefe600199c5
Marcos Souza <marcos.dev.ti@gmail.com>
2020-06-03 01:09:18 -0400

    adsense right list

--- src/TouchOnline.Api/ClientApp/src/app/pages/lessons/shared/seo-list/seo-list.component.ts
+++ src/TouchOnline.Api/ClientApp/src/app/pages/lessons/shared/seo-list/seo-list.component.ts
@@ -8,10 +8,30 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class SeoListComponent implements OnInit {
 lang: string;
+showPub: boolean;
   constructor(public translate: TranslateService) { }
 
   ngOnInit() {
     this.lang = this.translate.currentLang;
+    this.setPub();
+  }
+
+
+  setPub() {
+    const storedPub = localStorage.getItem('pub-list-left');
+    if(storedPub){
+      const num = Number.parseInt(storedPub);
+      if(num < 10){
+        localStorage.setItem('pub-list-left', (num +1).toString());
+        this.showPub = false;
+      } else {
+        localStorage.setItem('pub-list-left', '1')
+        this.showPub = true
+      }
+    } else {
+      localStorage.setItem('pub-list-left', '1')
+      this.showPub = true
+    }
   }
 
 }
